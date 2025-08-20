4eafbe47ebbc8a21fef4478a33314f19fe453f4f
darcyStephens <darcystephens40@gmail.com>
2025-08-20 21:25:29 +0930

    changing to use sigaction for even.c because i like learning

--- even.c
+++ even.c
@@ -1,51 +1,62 @@
-//Write a C program called even.c that takes the input parameter n from the command line and prints the first “n” even numbers (zero is even). 
-//We want the program to run slowly, so that it executes long enough to receive a signal.  
-//To achieve this, you should place a sleep(5) after every print statement.  
-//Compile and run to test it works ok.
+// Write a C program called even.c that takes the input parameter n from the command line and prints the first “n” even numbers (zero is even).
+// We want the program to run slowly, so that it executes long enough to receive a signal.
+// To achieve this, you should place a sleep(5) after every print statement.
+// Compile and run to test it works ok.
 
-//Most Unix systems will understand SIGHUP and SIGINT.  
-//Every signal has a default action associated with it, 
-//for example the default action for SIGINT is to terminate the process. 
+// Most Unix systems will understand SIGHUP and SIGINT.
+// Every signal has a default action associated with it,
+// for example the default action for SIGINT is to terminate the process.
 
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
-#include <string.h>  // for memset
-#include <stdlib.h>  // for atoi
+#include <string.h> // for memset
+#include <stdlib.h> // for atoi
 
 void signal_handler(int signal)
 {
-    //switch statement for when i have to extend to more signals
-    switch (signal) 
+    // switch statement for when i have to extend to more signals
+    switch (signal)
     {
-        case SIGHUP:
+    case SIGHUP:
         printf("Ouch!\n");
         break;
 
-        case SIGINT:
+    case SIGINT:
         printf("Yeah!\n");
         break;
-
     }
 }
 
-
-
 int main(int argc, char *argv[])
 {
+
+    //using sigaction because i wante to learn how to use it
+    //this was a foolish endevour.
+    struct sigaction sa;
+    sa.sa_flags = 0;
+    sa.sa_handler = signal_handler;
+
     int n = atoi(argv[1]);
 
-    //change the signal action
-    signal(SIGINT, &signal_handler);
-    signal(SIGHUP, &signal_handler);
+    // change the signal action
+    if (sigaction(SIGINT, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+    }
+    if (sigaction(SIGHUP, &sa, NULL) == -1)
+        perror("sigaction SIGHUP");
 
-    //print even numbers
-    for(int i = 0; i< n * 2; i = i+2)
+
+    // signal(SIGINT, &signal_handler);
+    //signal(SIGHUP, &signal_handler);
+
+    // print even numbers
+    for (int i = 0; i < n * 2; i = i + 2)
     {
         printf("%d \n", i);
         fflush(stdout);
         sleep(5);
     }
     return 0;
-   
 }
