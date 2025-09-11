22ce9bab2022f1ff674763a13365404eca07cb48
spb-59 <shreeshailpbhatta@gmail.com>
2025-09-12 08:34:14 +0930

    fix clock?

--- clockmmu.py
+++ clockmmu.py
@@ -1,11 +1,12 @@
 from mmu import MMU
 from collections import deque
-class ClockMMU(MMU):
 
-    def __init__(self,frames):
+
+class ClockMMU(MMU):
+    def __init__(self, frames):
         super().__init__(frames)
-        self.clock_hand=0
-        self.circle=deque(maxlen=frames)
+        self.clock_hand = 0
+        self.circle = deque(maxlen=frames)
 
     def replace(self, page_number):
         if page_number in self.tlb:
@@ -13,7 +14,7 @@ class ClockMMU(MMU):
             self.tlb[page_number] = True
             return 1
 
-        if len(self.tlb)<self.max_frames:
+        if len(self.tlb) < self.max_frames:
             self.log("Miss, space available adding to table")
             self.tlb[page_number] = True
             self.circle.append(page_number)
@@ -21,22 +22,22 @@ class ClockMMU(MMU):
                 self.dirty_pages.remove(page_number)
             self.log(f"  Loaded {page_number}")
             return -1
-        
-        start=self.clock_hand
+
+        start = self.clock_hand
         while True:
             victim = self.circle[self.clock_hand]
-            if not self.tlb[victim] :
-                return self._evict(victim,page_number)
+            if not self.tlb[victim]:
+                return self._evict(victim, page_number)
 
             else:
                 self.log(f"Use flag true giving second chance for {victim}")
                 self.tlb[victim] = False
                 self.clock_hand = (self.clock_hand + 1) % self.max_frames
-            
+
             self.log("Full circle with clock clear the start")
             if self.clock_hand == start:
                 victim = self.circle[self.clock_hand]
-                return self._evict(victim,page_number)
+                return self._evict(victim, page_number)
 
     def _evict(self, victim, page_number):
         was_dirty = victim in self.dirty_pages
@@ -50,12 +51,6 @@ class ClockMMU(MMU):
         self.log(f"  Evicted {victim} (dirty: {was_dirty})")
 
         if page_number in self.dirty_pages:
-            self.dirty_pages.remove(page_number) 
-            return -1
+            self.dirty_pages.remove(page_number)
 
         return 0 if was_dirty else -1
-
-
-
-
-        
