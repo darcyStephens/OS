60b41838c70c3fece4dbae47c7f18853f94475fd
darcyStephens <darcystephens40@gmail.com>
2025-10-17 11:26:03 +1030

    good plan

--- clockmmu.py
+++ clockmmu.py
@@ -9,30 +9,43 @@ class ClockMMU(MMU):
         self.circle = deque(maxlen=frames)
 
     def replace(self, page_number):
+        #return 1 if hit
+        #return -1 if miss
+        #return 0 if dirty miss
+
+        #hit set the page used bit to true in the TLB
         if page_number in self.tlb:
             self.log("Hit, set use flag")
             self.tlb[page_number] = True
             return 1
 
+        #forced miss, space available and page not in frame
         if len(self.tlb) < self.max_frames:
             self.log("Miss, space available adding to table")
             self.tlb[page_number] = True
+            #add page to queue
             self.circle.append(page_number)
             self.log(f"  Loaded {page_number}")
             return -1
 
         start = self.clock_hand
         while True:
+            #get the page at the front of the queue
             victim = self.circle[self.clock_hand]
             if not self.tlb[victim]:
+                #page used bit is false, remove page
                 return self._evict(victim, page_number)
 
             else:
+                #page used bit is true
                 self.log(f"Use flag true giving second chance for {victim}")
+                #set to false
                 self.tlb[victim] = False
+                #increment the clock
                 self.clock_hand = (self.clock_hand + 1) % self.max_frames
 
             self.log("Full circle with clock clear the start")
+            #everything has been used just remove whatever is at the front
             if self.clock_hand == start:
                 victim = self.circle[self.clock_hand]
                 return self._evict(victim, page_number)
--- lrummu.py
+++ lrummu.py
@@ -8,29 +8,41 @@ class LruMMU(MMU):
 
 
     def replace(self, page_number):
-    
+        #return 1 if hit
+        #return -1 if miss
+        #return 0 if dirty miss
+
+        #hit a page in the page table
         if page_number in self.tlb:
             self.log(f"HIt updating for page {page_number}")
             self.tlb[page_number] = time.time_ns()
             return 1
         
+        #forced missed, tlb not full and page not already there
         if len(self.tlb) < self.max_frames: 
+            #key is the page number, value is time of insertion
             self.tlb[page_number] = time.time_ns()
             return -1
+        
         
         if len(self.tlb) == self.max_frames:
             oldest_time = float('inf')
             oldest_key = -1
+
+            #find the least recently used (LRU) page in the TLB
             for key, access_time in self.tlb.items():
                 if access_time < oldest_time:
                     oldest_time = access_time
                     oldest_key = key
             
             if oldest_key != -1:
+                #remove LRU from tlb
                 del self.tlb[oldest_key]
+                #add in new page to replace old one
                 self.tlb[page_number] = time.time_ns()
             
             if oldest_key in self.dirty_pages:
+                #page to remove was dirty
                 self.dirty_pages.remove(oldest_key)
                 return 0
         
